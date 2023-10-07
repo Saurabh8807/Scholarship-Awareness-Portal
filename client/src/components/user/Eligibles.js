aa2744197d1aebf4a928f99cd8e5108619611a88
Saurabh Jagtap <sjagtap18092002@gmail.com>
2023-10-07 14:52:17 +0530

    government feature added

--- client/src/components/user/Eligibles.js
+++ client/src/components/user/Eligibles.js
@@ -22,29 +22,29 @@ const NeedBased = () => {
     const [scholarship, setScholarship] = useState([]);
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-      // Fetch data from your API here (replace with your API endpoint)
-      fetch('https://api.example.com/colleges')
-        .then((response) => response.json())
-        .then((data) => setData(data));
-    }, []);
+    // useEffect(() => {
+    //   // Fetch data from your API here (replace with your API endpoint)
+    //   fetch('https://api.example.com/colleges')
+    //     .then((response) => response.json())
+    //     .then((data) => setData(data));
+    // }, []);
     // Get function
-    const getScholarships = async () => {
-      try {
-        const { data } = await axios.get(
-          `http://localhost:8080/get-scholarships`
-        );
-        setScholarship(data.scholarship);
+    // const getScholarships = async () => {
+    //   try {
+    //     const { data } = await axios.get(
+    //       `http://localhost:8080/get-scholarships`
+    //     );
+    //     setScholarship(data.scholarship);
         
-      } catch (error) {
-        console.log(error);
-        alert("Something went wrong");
-      }
-    };
+    //   } catch (error) {
+    //     console.log(error);
+    //     alert("Something went wrong");
+    //   }
+    // };
   
-    useEffect(() => {
-      getScholarships();
-    }, []);
+    // useEffect(() => {
+    //   getScholarships();
+    // }, []);
   
     // const [show, setShow] = useState(false);
   
@@ -59,6 +59,31 @@ const NeedBased = () => {
       setShowModal(true);
     };
   
+    const staticData = [
+      { collegeName: 'BITS Pilani', amount: getRandomAmount() },
+      { collegeName: 'VIT Vellore', amount: getRandomAmount() },
+      { collegeName: 'SRM University', amount: getRandomAmount() },
+      { collegeName: 'Manipal Institute of Technology', amount: getRandomAmount() },
+      { collegeName: 'Amity University', amount: getRandomAmount() },
+      { collegeName: 'Lovely Professional University', amount: getRandomAmount() },
+      { collegeName: 'Symbiosis International University', amount: getRandomAmount() },
+      { collegeName: 'Thapar Institute of Engineering and Technology', amount: getRandomAmount() },
+      { collegeName: 'Jain University', amount: getRandomAmount() },
+      { collegeName: 'Christ University', amount: getRandomAmount() },
+      { collegeName: 'Birla Institute of Technology, Mesra', amount: getRandomAmount() },
+      { collegeName: 'Manipal University Jaipur', amount: getRandomAmount() },
+      { collegeName: 'Kalinga Institute of Industrial Technology', amount: getRandomAmount() },
+      { collegeName: 'Shiv Nadar University', amount: getRandomAmount() },
+      { collegeName: 'Galgotias University', amount: getRandomAmount() },
+      { collegeName: 'SASTRA University', amount: getRandomAmount() },
+      { collegeName: 'PES University', amount: getRandomAmount() },
+    ];
+    
+    function getRandomAmount() {
+      return Math.floor(Math.random() * 10000) + 5000; // Random amount between 5000 and 14999
+    }
+    
+    // This array contains 17 Indian private college names with random amounts.
     
   
     // const [scholarshipName , setScholarshipName] = useState("");
@@ -68,8 +93,8 @@ const NeedBased = () => {
         <div className="box-content">
           <div className="recommended-tag">Recommended</div>
           <div className="college-info">
-            <strong>College Name :--</strong>
-            <span>"......"</span>
+            <strong>College Name :-- </strong>
+            <span>Thapar Institute of Engineering and Technology</span>
           </div>
           <div className="accuracy-info">
           <strong>Sponsorship :--</strong>
@@ -84,7 +109,7 @@ const NeedBased = () => {
         <div className="box-content">
           <div className="college-info">
             <strong>College Name :--</strong>
-            <span>"......"</span>
+            <span> Shiv Nadar University</span>
           </div>
           <div className="accuracy-info">
             <strong>Sponsorship :--</strong>
@@ -93,7 +118,7 @@ const NeedBased = () => {
         </div>
       </div>
 <div className="table-container">
-      <table className="custom-table">
+<table className="custom-table">
         <thead>
           <tr>
             <th>College Name</th>
@@ -102,12 +127,12 @@ const NeedBased = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
+          {staticData.map((item, index) => (
             <tr key={index}>
               <td>{item.collegeName}</td>
               <td>${item.amount}</td>
               <td>
-                <button className="action-button">Action</button>
+                <button className="action-button">Apply Now</button>
               </td>
             </tr>
           ))}
